61a7dfca39babbd44df22d301f9fa9a107908644
Christian Jose Atencia Caro <Catencia@intergrupo.com>
2021-08-29 13:48:52 -0500

    Add Consolidated Entity

---
+++ TimeAzureFunction.Common/Models/Consolidated.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TimeAzureFunction.Common.Models
+{
+    public class Consolidated
+    {
+        public int EmployeId { get; set; }
+
+        public DateTime Time { get; set; }
+
+        public int minutesWorked { get; set; }
+    }
+}
---
+++ TimeAzureFunction.Function/Entities/ConsolidatedEntity.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+
+namespace TimeAzureFunction.Function.Entities
+{
+    public class ConsolidatedEntity : TableEntity
+    {
+        public int EmployeId { get; set; }
+
+        public DateTime Time { get; set; }
+
+        public int minutesWorked { get; set; }
+    }
+}
